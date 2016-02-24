8c904d248d26ab90c9174d3767e995ada6157e68
Nrupen Desai <nrupen92@gmail.com>
2016-02-24 04:55:23 -0800

    sdfasdf

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res){
-	  res.sendFile(__dirname + '/client.html');
+	  res.sendFile(__dirname + '/index.html');
 	});
 
 module.exports = router;
